3dd45370aca667d6813adfc315f55ea39e040bcf
Young <youngheesomemoto@gmail.com>
2024-02-01 22:16:42 -0600

    sign up compenent step 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Authenticate from './components/Authenitcate'
+import SignUpForm from './components/SignUpForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Authenticate />
+      <SignUpForm />
     </>
   )
 }
